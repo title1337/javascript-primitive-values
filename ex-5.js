da79fe20c33f8ffb27771d4a00b87ac678b77ded
Tle <title1337@gmail.com>
2026-03-03 10:23:31 +0700

    fix: Ex5

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let price = 27000;
+let discount = (25 / 100) * price;
+console.log(discount);
+let discountedPrice = price - discount;
+console.log(discountedPrice);
+
